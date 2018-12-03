3a41010390e1abdb015923d275f94ab6680404bc
Christopher Gentile (CMGENT4344) <CMGENT4344@ung.edu>
2018-12-03 17:27:05 -0500

    Added Movie list for game

---
+++ MovieDictionary.py
@@ -0,0 +1,11 @@
+import random
+ChristmasMovie = {'christmas vacation': ['cousion eddie', 'griswolds', 'shitters full', 'chevy chase', 'eddie', 'clarke', 'dry turkey', 'christmas lights', 'beverly deangelo', 'dead cat'],
+'home alone': ['kevin', 'booby traps', 'wet bandits', 'robbers', 'ya filthy animal', 'loud scream', 'macaulay culkin', 'family left'],
+'elf': ['buddy', 'will ferrel', 'sugar', 'zoey deschanel', 'new york', 'maple syrup', 'naughty list'],
+'how the grinch stole christmas': ['grinch', 'max', 'whoville', 'jim carrey', 'green', 'grumpy', 'dr.seuss', 'anthony hopkins', 'cindy lou'],
+'the nightmare before christmas': ['disney', 'jack skellington', 'halloween', 'oogie boogie', 'sally', 'jack', 'tim burton', 'halloween town', 'pumpkin king', 'this is halloween', 'christmas town', 'dr.finklestien'],
+'a christmas story': ['ralphie', 'red ryder', 'shoot your eye out', 'leg lamp', 'bully', 'glasses', 'air rifle', 'fa rah rah', 'flick'],
+'bad santa': ['billy bob thorton', 'mall santa', 'willie', 'make you some sandwiches', 'bernie mac', 'drunk', 'robbery'],
+'jingle all the way': ['arnold', 'turbo man', 'sinbad', 'shopping', 'santa fight', 'booster', 'drunk reindeer', 'bouncy ball'],
+'the santa clause': ['kills santa', 'tim allen', 'santa suit', 'north pole', 'scott calvin', 'bernard'],
+'a charlie brown christmas': ['chralie brown', 'snoopy', 'peanuts', 'charles schulz', 'little tree', 'meaning of christmas']}
